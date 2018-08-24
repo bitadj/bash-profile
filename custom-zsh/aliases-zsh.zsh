af56256fea682b61b1b386e82eeb0048c7690cbf
Bita Djaghouri <bita@bitadj.com>
2018-08-24 17:30:20 -0400

    update settings for vscode

--- custom-zsh/aliases-zsh.zsh
+++ custom-zsh/aliases-zsh.zsh
@@ -19,10 +19,9 @@ alias sb='sourcebash'
 alias gt='gitalias'
 
 # display aliases
-alias zshrc='code ~/.zshrc'
 alias al='dog $ZSH_CUSTOM/aliases-zsh.zsh'
 alias diral='dog $ZSH_CUSTOM/diraliases.zsh'
-alias gitalias='dog $ZSH_CUSTOM/git-aliases.zsh'
+alias gital='dog $ZSH_CUSTOM/git-aliases.zsh'
 alias uu='dog $ZSH_CUSTOM/diraliases-uu.zsh'
 alias aws='dog $ZSH_CUSTOM/local-alias.zsh'
 
